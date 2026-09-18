67103f9f3cf7b85c94a968a67bf2a912d1a68685
Manal <manalattia80-web@github.com>
2026-09-18 15:00:14 +0300

    Fix CardThemeData for Flutter 3.47+

--- mobile/lib/core/theme/app_theme.dart
+++ mobile/lib/core/theme/app_theme.dart
@@ -47,7 +47,7 @@ class AppTheme {
       ),
 
       // Cards
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color:       AppColors.bgCard,
         elevation:   0,
         shape:       RoundedRectangleBorder(
